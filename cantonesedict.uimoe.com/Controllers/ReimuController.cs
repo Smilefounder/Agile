0299b4341fc12dbf19bb4fbae890de8554f8be46
llyn23 <i@llyn23.com>
2016-07-26 17:49:59 +0800

    cantonesedict - 加入后台管理框架

---
+++ cantonesedict.uimoe.com/Controllers/ReimuController.cs
@@ -0,0 +1,48 @@
+﻿using Agile.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cantonesedict.uimoe.com.Controllers
+{
+    [FreeAccess]
+    public class ReimuController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Statistics()
+        {
+            return View();
+        }
+
+        public ActionResult Vocabulary()
+        {
+            return View();
+        }
+
+        public ActionResult AddVocabulary()
+        {
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        public ActionResult UserList()
+        {
+            return View();
+        }
+    }
+}

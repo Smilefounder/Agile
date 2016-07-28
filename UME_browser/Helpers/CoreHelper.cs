91300d9dca0370ddd4dc24b498f33ff9e2e7b906
llyn23 <i@llyn23.com>
2016-07-28 23:36:36 +0800

    UME_browser - 完成地址栏输入判断

---
+++ UME_browser/Helpers/CoreHelper.cs
@@ -0,0 +1,74 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UME_browser.Helpers
+{
+    public class CoreHelper
+    {
+        public static Grid _mainGrid;
+
+        public static void Render(HtmlDocument doc)
+        {
+            var br = new Border();
+            br.Background = System.Windows.Media.Brushes.Gray;
+            _mainGrid.Children.Add(br);
+        }
+
+        private static Dictionary<string, Action> _keywords = null;
+
+        private static Dictionary<string, Action> Keywords
+        {
+            get
+            {
+                if (_keywords == null)
+                {
+                    _keywords = new Dictionary<string, Action>();
+                    _keywords.Add("ume:about", About);
+                }
+
+                return _keywords;
+            }
+        }
+
+        public static void Navigate(string address)
+        {
+            var pattern1 = "^http://([0-9a-zA-Z]+[.])+[0-9a-zA-Z]+";
+            if (Regex.IsMatch(address, pattern1))
+            {
+                LoadHtml(address);
+                return;
+            }
+
+            if (!Keywords.ContainsKey(address))
+            {
+                MessageBox.Show("无效的指令");
+                return;
+            }
+
+            var act = Keywords[address];
+            act.Invoke();
+        }
+
+        public static void LoadHtml(string url)
+        {
+            var html = new WebClient().DownloadString(url);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            Render(doc);
+        }
+
+        public static void About()
+        {
+            MessageBox.Show("UME_browser");
+        }
+    }
+}
--- UME_browser/MainWindow.xaml.cs
+++ UME_browser/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UME_browser.Helpers;
 
 namespace UME_browser
 {
@@ -23,6 +24,37 @@ namespace UME_browser
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            CoreHelper._mainGrid = _mainGrid;
+        }
+
+        private void _navigateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Navigate();
+        }
+
+        private void _addressTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Navigate();
+            }
+        }
+
+        private void Navigate()
+        {
+            var address = _addressTextBox.Text.Trim();
+            if (string.IsNullOrEmpty(address))
+            {
+                _addressTextBox.Focus();
+                return;
+            }
+
+            CoreHelper.Navigate(address);
         }
     }
 }
